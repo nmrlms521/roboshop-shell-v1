22623d0a3f4c50b6dde7ec8d87bd4ab6cdea89b7
mohan <nerjimohanarao@gmail.com>
2025-11-18 22:28:38 +0530

    frontend script - modified of restart

--- frontend.sh
+++ frontend.sh
@@ -5,11 +5,13 @@ dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
+rm -f /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf
+
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-rm -f /etc/nginx/nginx.conf
-cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx
 systemctl restart nginx
