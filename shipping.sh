38504535126c532002b395382f96a8e67502540a
mohan <nerjimohanarao@gmail.com>
2025-11-19 19:25:27 +0530

    shiiping issue fixed

--- shipping.sh
+++ shipping.sh
@@ -13,17 +13,11 @@ cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-
-
 dnf install mysql -y
-
 mysql -h mysql-dev.smks.online -uroot -pRoboShop@1 < /app/db/schema.sql
-
 mysql -h mysql-dev.smks.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-
 mysql -h mysql-dev.smks.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl daemon-reload
-
 systemctl enable shipping
 systemctl restart shipping
