746edd31ffe1d11c38d04adddbc3b3e28271519c
mohan <nerjimohanarao@gmail.com>
2025-11-19 17:00:51 +0530

    catalogue issue fixed

--- catalogue.sh
+++ catalogue.sh
@@ -4,8 +4,9 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 useradd roboshop
-cp catalogue.service etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 mkdir /app
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
